346076185114e36c4e35cdbdaadf654e357b812b
Andrew Chepreghy <andris788@gmail.com>
2019-05-23 19:20:08 +0200

    separated loading from query and local csv

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 
+from src.import_data import get_gama_dataset_from_query, get_gama_dataset_from_csv
 from src.neural_network_classifier import run_neural_network
 
-df = pd.read_hdf('train.h5')
+# df = pd.read_hdf('train.h5')
+# model = run_neural_network(df, batch_size=5, hidden_layers=[128, 128], n_epochs=5)
 
-model = run_neural_network(df, batch_size=5, hidden_layers=[128, 128], n_epochs=5)
-print('model', model)
+gama_df = get_gama_dataset_from_csv()
+print(gama_df.head())
--- src/import_data.py
+++ src/import_data.py
@@ -2,11 +2,15 @@ from astroquery.gama import GAMA
 import pandas as pd
 import numpy as np
 
-def get_gama_dataset():
+def get_gama_dataset_from_query():
   gama_all_data = GAMA.query_sql('SELECT * FROM AATSpecAll AS s WHERE s.z BETWEEN 0.000000001 AND 10')
   gama_all_data_df = pd.DataFrame(np.array(gama_all_data)) # convert table to CSV
-  print('gamma_all_data_df.head()', gama_all_data_df.head())
   gama_all_data_df.to_csv('data/GAMA.csv') # write csv into data folder
 
   return gama_all_data_df
 
+
+def get_gama_dataset_from_csv():
+  gama_df = pd.read_csv('data/GAMA.csv')
+  
+  return gama_df
