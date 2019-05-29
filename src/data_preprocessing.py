95778ac7d8c12c34a96a41f6f28fa0f6e6f23ed5
Andrew Chepreghy <andris788@gmail.com>
2019-05-29 10:59:10 +0200

    minor changes

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,7 +1,18 @@
 import numpy as np
 from astropy.io import fits
+import urllib.request
+import pandas as pd
 
-fits_image_filename = fits.util.get_testdata_filepath('http://www.gama-survey.org/dr3/data/spectra/gama/reduced_27/1d/G02_Y3_001_004.fit')
-hdul = fits.open(fits_image_filename)
-print(hdul.info())
-hdul.close()
+url = 'http://www.gama-survey.org/dr3/data/spectra/gama/reduced_27/1d/G02_Y3_001_004.fit'
+urllib.request.urlretrieve(url, 'G02_Y3_001_004.fit')
+
+# fits_image_filename = fits.util.get_testdata_filepath('G02_Y3_001_004.fit')
+hdul = fits.open('G02_Y3_001_004.fit')
+
+with fits.open('G02_Y3_001_004.fit') as data:
+  df = pd.DataFrame(data[0].data)
+
+  for index, item in enumerate(data[0].header):
+    print(item, data[0].header[index])
+
+print(df.head())
