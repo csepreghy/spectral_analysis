2449b9b3d43e96727dcb228859fd40360c44204a
Zoe Ansari <zoemc.ansari@gmail.com>
2019-06-03 14:42:33 +0200

    try

--- src/import_data.py
+++ src/import_data.py
@@ -8,11 +8,14 @@ from astropy.table import Table
 from astroquery.sdss import SDSS
 import astropy.units as u
 from astropy import coordinates as coords
+from astropy.table import Table
 
 import pickle
 
 
 def get_save_SDSS_from_coordinates(ra, dec):
+    n=0
+
     df = {}
     df['flux_list'] = []  # ': flux,
     df['wavelength'] = []  #: wavelength,
@@ -28,48 +31,115 @@ def get_save_SDSS_from_coordinates(ra, dec):
     print(length)
     number_none = 0
     while counter < int(length):
-        counter += 1
-        pos = coords.SkyCoord((ra[counter]) * u.deg, (dec[counter]) * u.deg, frame='icrs')
-        xid = SDSS.query_region(pos, spectro=True)#, radius=5 * u.arcsec)
+# <<<<<<< HEAD
+        try:
+            counter += 1
+            pos = coords.SkyCoord((ra[counter]) * u.deg, (dec[counter]) * u.deg, frame='icrs')
+            xid = SDSS.query_region(pos, spectro=True)#, radius=5 * u.arcsec)
 
-        #         print(xid)
-        #         print(counter)
-        #         print(type(xid))
+            #         print(xid)
+            #         print(counter)
+            #         print(type(xid))
 
-        #         typ="<class 'NoneType'>"
-        if xid != None:
-            #             print(len(xid))
+            #         typ="<class 'NoneType'>"
+            # print("type:",type(xid))
+            # print(counter)
+            # print(len(xid))
+            if xid != None:
 
-            if len(xid) > 1:
-                xid = Table(xid[0])
-            elif len(xid) == 1:
-                xid = xid
-        else:
-            number_none =+1
-            print(counter)
-            continue
+                if len(xid) > 1:
+                    # print(len(xid))
+                    # print(xid[0])
+                    # print(xid)
+                    xid = Table(xid[0])
+                elif len(xid) == 1:
+                    xid = xid
+            elif xid == None:
+                # print("here")
+                number_none = +1
+                print(counter)
+                continue
 
-        #         print(type(xid))
-        #         print(counter)
+            # print("this is first row:", xid[0])
+            # print(type(xid))
 
-        sp = SDSS.get_spectra(matches=xid)
-        plate_tab = xid["plate"]
-        plate = plate_tab[0]
-        #         print(plate)
-        so = SDSS.query_specobj(plate=plate, fields=['ra', 'dec', 'z', 'zErr'])
-        #         print("here")
+            # print(xid)
 
-        df['flux_list'].append(sp[0][1].data['flux'])
-        df['wavelength'].append(10. ** sp[0][1].data['loglam'])
-        df['z'].append(xid['z'])
-        df['ra'].append(xid['ra'])
-        df['dec'].append(xid['dec'])
-        df['objid'].append(xid['objid'])
-        #         df['coordinate'].append(coordinate)
-        df['zErr'].append(so['zErr'])
+            #         print(type(xid))
+            #         print(counter)
 
-    #         print(counter , "Done")
+            sp = SDSS.get_spectra(matches=xid)
+            plate_tab = xid["plate"]
+            plate = plate_tab[0]
+            #         print(plate)
+            so = SDSS.query_specobj(plate=plate, fields=['ra', 'dec', 'z', 'zErr'])
+            #         print("here")
+
+            df['flux_list'].append(sp[0][1].data['flux'])
+            df['wavelength'].append(10. ** sp[0][1].data['loglam'])
+            df['z'].append(xid['z'])
+            df['ra'].append(xid['ra'])
+            df['dec'].append(xid['dec'])
+            df['objid'].append(xid['objid'])
+            #         df['coordinate'].append(coordinate)
+            df['zErr'].append(so['zErr'])
+
+        except:
+            n = +1
 
     df = pd.DataFrame(df)
     df.to_pickle('data/sdss/'+str(length)+'.pkl')
     print("number of None values for Xid is:", number_none)
+
+        #         print(counter , "Done")
+# # =======
+#         counter += 1
+#         pos = coords.SkyCoord((ra[counter]) * u.deg, (dec[counter]) * u.deg, frame='icrs')
+#         xid = SDSS.query_region(pos, spectro=True)#, radius=5 * u.arcsec)
+#
+#         #         print(xid)
+#         #         print(counter)
+#         #         print(type(xid))
+#
+#         #         typ="<class 'NoneType'>"
+#         if xid != None:
+#             #             print(len(xid))
+#
+#             if len(xid) > 1:
+#                 xid = Table(xid[0])
+#             elif len(xid) == 1:
+#                 xid = xid
+#         else:
+#             number_none =+1
+#             print(counter)
+#             continue
+#
+#         #         print(type(xid))
+#         #         print(counter)
+#
+#         sp = SDSS.get_spectra(matches=xid)
+#         plate_tab = xid["plate"]
+#         plate = plate_tab[0]
+#         #         print(plate)
+#         so = SDSS.query_specobj(plate=plate, fields=['ra', 'dec', 'z', 'zErr'])
+#         #         print("here")
+#
+#         df['flux_list'].append(sp[0][1].data['flux'])
+#         df['wavelength'].append(10. ** sp[0][1].data['loglam'])
+#         df['z'].append(xid['z'])
+#         df['ra'].append(xid['ra'])
+#         df['dec'].append(xid['dec'])
+#         df['objid'].append(xid['objid'])
+#         #         df['coordinate'].append(coordinate)
+#         df['zErr'].append(so['zErr'])
+#
+#     #         print(counter , "Done")
+# # >>>>>>> c39b09ef5d75b2cb6af720d5e37fbb5c1cdcdf69
+#
+
+# # <<<<<<< HEAD
+
+
+
+# =======
+# >>>>>>> c39b09ef5d75b2cb6af720d5e37fbb5c1cdcdf69
