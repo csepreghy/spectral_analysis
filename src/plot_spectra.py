555c55966f396b83ef610b836343a9ccc3c5e6ef
Andrew Chepreghy <andris788@gmail.com>
2019-06-06 12:56:52 +0200

    cleaned up plotting file a little

--- src/plot_spectra.py
+++ src/plot_spectra.py
@@ -7,22 +7,21 @@ from scipy import ndimage
 
 from plotify import Plotify
 
+# --- Initialize variables --- #
 plotify = Plotify()
 spectra = pd.read_pickle('data/sdss/FinalTable.pkl')
-
-def apply_gaussian_filter(fluxes, sigma):
-  return filters.gaussian(image=fluxes, sigma=sigma)
-
 z = spectra.get_values()[:,3]
 fluxes = spectra.get_values()[:,0]
 wavelengths = spectra.get_values()[:, 1]
 gaussian_sigma = 64
-
 spectrum_x = spectra.iloc[42]['wavelength']
 spectrum_y = spectra.iloc[42]['flux_list']
-print('spectra.iloc[42][ra]', type(spectra.iloc[42]['ra']))
 spectrum_title = 'Spectrum with guassian smoothing, sigma = ' + str(gaussian_sigma)
 filename = 'gaussian-' + str(gaussian_sigma)
+
+def apply_gaussian_filter(fluxes, sigma):
+  return filters.gaussian(image=fluxes, sigma=sigma)
+
 
 spectrum_y = apply_gaussian_filter(spectrum_y, sigma=gaussian_sigma)
 spectrum_y = spectrum_y[::8]
