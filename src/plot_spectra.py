dcd163ee338cd3abf722ee9a8fae3465b9490ea9
Andrew Chepreghy <andris788@gmail.com>
2019-06-06 12:52:01 +0200

    refactored spectrum plotting and gaussian filtering

--- src/plot_spectra.py
+++ src/plot_spectra.py
@@ -8,36 +8,27 @@ from scipy import ndimage
 from plotify import Plotify
 
 plotify = Plotify()
-spectra = pd.read_pickle('data/sdss/60001-65000.pkl')
+spectra = pd.read_pickle('data/sdss/FinalTable.pkl')
 
+def apply_gaussian_filter(fluxes, sigma):
+  return filters.gaussian(image=fluxes, sigma=sigma)
 
-print(spectra.iloc[0])
 
 z = spectra.get_values()[:,3]
 fluxes = spectra.get_values()[:,0]
 wavelengths = spectra.get_values()[:, 1]
+gaussian_sigma = 64
 
-spectrum_x = spectra.iloc[0]['wavelength']
-spectrum_y = spectra.iloc[0]['flux_list']
-print('spectra.iloc[0][ra]', type(spectra.iloc[0]['ra']))
-spectrum_title = 'Spectrum for coordinates: ' + str(spectra.iloc[0]['ra']) + ', ' +  str(spectra.iloc[0]['dec'])
+spectrum_x = spectra.iloc[42]['wavelength']
+spectrum_y = spectra.iloc[42]['flux_list']
+print('spectra.iloc[42][ra]', type(spectra.iloc[42]['ra']))
+spectrum_title = 'Spectrum with guassian smoothing, sigma = ' + str(gaussian_sigma)
+filename = 'gaussian-' + str(gaussian_sigma)
 
-
-print('spectrum_x', spectrum_x)
-print('spectrum_y', spectrum_y)
-
-# plotify.plot(
-#   x_list = spectrum_x, 
-#   y_list = spectrum_y,
-#   xlabel = 'Frequencies (Hz)',
-#   ylabel = 'Flux'
-# )
-
-def apply_gaussian_filter(fluxes, sigma = 2):
-  return filters.gaussian(image=fluxes, sigma=sigma)
-
-spectrum_y = apply_gaussian_filter(spectrum_y, sigma=4)
-
+spectrum_y = apply_gaussian_filter(spectrum_y, sigma=gaussian_sigma)
+spectrum_y = spectrum_y[::8]
+spectrum_x = spectrum_x[::8]
+print('len(spectrum_y)', len(spectrum_y))
 
 fig, ax = plotify.plot(
   x_list = spectrum_x,
@@ -45,6 +36,7 @@ fig, ax = plotify.plot(
   xlabel = 'Frequencies (Hz)',
   ylabel = 'Flux',
   title = spectrum_title,
-  figsize=(12, 8)
+  figsize=(12, 8),
+  show_plot=True,
+  filename=filename
 )
-
