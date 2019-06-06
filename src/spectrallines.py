6ad8d4f66537f0438a3fe6ba718c2d8ecd9c3447
Andrew Chepreghy <andris788@gmail.com>
2019-06-06 14:37:40 +0200

    changed variable names to lowercase

--- src/spectrallines.py
+++ src/spectrallines.py
@@ -1,9 +1,9 @@
 from __future__ import division
 import numpy as np
 
-QSO = ['Lya_em', 'C4_ab', 'C4_ab', 'Lyb_ab']
-GAL = ['OII_em', 'OIII_em', 'OIII_em', 'Na_ab', 'Mg_ab']
-Star = ['HeI_ab', 'FeII_ab', 'FeII_ab', 'FeII_ab', 'CAHK_ab', 'CAHK_ab']
+qso = ['Lya_em', 'C4_ab', 'C4_ab', 'Lyb_ab']
+gal = ['OII_em', 'OIII_em', 'OIII_em', 'Na_ab', 'Mg_ab']
+star = ['HeI_ab', 'FeII_ab', 'FeII_ab', 'FeII_ab', 'CAHK_ab', 'CAHK_ab']
 
 wl_qso = [1215, 1548, 1551, 1026]
 wl_gal = [3727, 4959, 5007, 5892, 5175]
