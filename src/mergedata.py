abef48df3ef2909c7d941862b0349fd9f6608b2f
Cecilie <ceciliehede@gmail.com>
2019-06-07 11:58:17 +0200

    quasar lines update

--- src/mergedata.py
+++ src/mergedata.py
@@ -5,15 +5,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 with open('data/FinalTable_Nikki(0-10000).pkl', 'rb') as f:
-        x = pickle.load(f)
+    x = pickle.load(f)
 
 with open('data/FinalTable_10-15-Zoe.pkl', 'rb') as g:
     y = pickle.load(g)
 
-#print(x['flux_list'].tolist())
+newlist = pd.concat([x, y], ignore_index=True)
+print(newlist)
 
-flux = x['flux_list'].tolist()
-wl = x['wavelength'].tolist()
+#flux = x['flux_list'].tolist()
+#wl = x['wavelength'].tolist()
 
-plt.plot(wl[0], flux[0])
-plt.show()
+#plt.plot(wl[0], flux[0])
+#plt.show()
