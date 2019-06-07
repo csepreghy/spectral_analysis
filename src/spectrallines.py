e318b4a4ea9423e1694a85cb2b322dd3e90241bb
Cecilie <ceciliehede@gmail.com>
2019-06-07 14:13:03 +0200

    quasar lines update + merge data function added

--- src/spectrallines.py
+++ src/spectrallines.py
@@ -1,10 +1,10 @@
 from __future__ import division
 import numpy as np
 
-qso = ['Lya_em', 'C4_ab', 'C4_ab', 'Lyb_ab', 'MgII_em', 'Hgamma_em']
+qso = ['Lya_em', 'C4_ab', 'C4_ab', 'Lyb_ab', 'MgII_em', 'Hgamma_em', 'Hbeta_em']
 gal = ['OII_em', 'OIII_em', 'OIII_em', 'Na_ab', 'Mg_ab', 'balmer_break']
 star = ['HeI_ab', 'FeII_ab', 'FeII_ab', 'FeII_ab', 'CAHK_ab', 'CAHK_ab']
 
-wl_qso = [1215, 1548, 1551, 1026, 2799, 4342]
+wl_qso = [1215, 1548, 1551, 1026, 2799, 4342, 4863]
 wl_gal = [3727, 4959, 5007, 5892, 5175, 4000]
 wl_star = [6678, 2600, 2383, 2374, 3934, 3969]
