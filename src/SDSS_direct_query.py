d88761a8bb0d6338f9478d2c464ee621aeb08225
Andrew Chepreghy <andris788@gmail.com>
2019-06-07 10:07:09 +0200

    formatting

--- src/SDSS_direct_query.py
+++ src/SDSS_direct_query.py
@@ -7,22 +7,22 @@ import time as time
 #
 # print(length)
 def query ():
-    start=time.time()
+  start=time.time()
 
-    query = "select top 100000  \
-    z, ra, dec, bestObjID, plate , class, zErr  \
-    from specObj \
-    where 1.0 >z   and \
-    z >0 and \
-    zWarning = 0"
-    res = SDSS.query_sql(query)
+  query = "select top 100000  \
+  z, ra, dec, bestObjID, plate , class, zErr  \
+  from specObj \
+  where 1.0 >z   and \
+  z >0 and \
+  zWarning = 0"
+  res = SDSS.query_sql(query)
 
-    df=res.to_pandas()
-    df_lowz=pd.DataFrame(df["ra"])
-    df_lowz["dec"]=df["dec"]
+  df=res.to_pandas()
+  df_lowz=pd.DataFrame(df["ra"])
+  df_lowz["dec"]=df["dec"]
 
-    df_lowz.to_csv('data/lowz.csv')
-    end=time.time()
-    df.to_pickle('data/sdss/direct_sql.pkl')
-    tt=end - start
-    print("time consuming:", tt)
+  df_lowz.to_csv('data/lowz.csv')
+  end=time.time()
+  df.to_pickle('data/sdss/direct_sql.pkl')
+  tt=end - start
+  print("time consuming:", tt)
