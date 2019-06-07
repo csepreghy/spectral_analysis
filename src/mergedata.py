f91cbe2331faa02bedbd797d168aaa5bd4baa644
Cecilie <ceciliehede@gmail.com>
2019-06-07 21:10:24 +0200

    Merge all data function done and tested

--- src/mergedata.py
+++ src/mergedata.py
@@ -7,13 +7,9 @@ import matplotlib.pyplot as plt
 def merge_data(filenames):
     with open('data/'+filenames[0]+'.pkl', 'rb') as g:
                 data = pickle.load(g)
-    for i in range(len(filenames)):
+    for i in range(1,len(filenames)):
         with open('data/'+filenames[i]+'.pkl', 'rb') as f:
             x = pickle.load(f)
         datalist = pd.concat([data, x], ignore_index=True)
         data = datalist
     return data
-
-names = ['FinalTable_Nikki(0-10000)', 'FinalTable_10-15-Zoe']
-alldata = merge_data(names)
-#print(alldata)
