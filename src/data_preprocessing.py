a1e3eb2efda287f7fe018e55472abf62541d3520
Andrew Chepreghy <andris788@gmail.com>
2019-06-08 19:40:53 +0200

    solved merge conflict

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import sys
 from skimage import io, filters, feature
 from scipy import ndimage
+from itertools import islice
 
 from plotify import Plotify
 
@@ -46,14 +47,65 @@ def plot_one_spectrum(spectra, nth_element, sigma, downsize, filename, save):
   )
 
 
-plot_one_spectrum(spectra=spectra, nth_element=41, sigma=4, downsize=1, filename='helloka', save=False)
+# plot_one_spectrum(spectra=spectra, nth_element=41, sigma=4, downsize=1, filename='helloka', save=False)
 
+# 3845 is the max
 
-def process_all_objects(spectra=spectra, sigma=16, downsize=8):
+def cutoff(spectra=spectra, sigma=16, downsize=8):
   min_wavelength_values = []
   max_wavelength_values = []
 
+  df = spectra
+
+  # for index, spectrum in islice(spectra.iterrows(), 5000):
+  #   min_value = np.amin(spectrum['wavelength'].tolist())
+  #   max_value = np.amax(spectrum['wavelength'].tolist())
+
+  #   if min_value < 3850: min_wavelength_values.append(min_value)
+  #   if max_value > 9100: max_wavelength_values.append(max_value)
+
+  # absolute_min = np.amax(np.array(min_wavelength_values))
+  # absolute_max = np.amin(np.array(max_wavelength_values))
+
+
+  rows_after_cutoff = []
+
   for index, spectrum in spectra.iterrows():
+    min_value = np.amin(spectrum['wavelength'].tolist())
+    max_value = np.amax(spectrum['wavelength'].tolist())
+
+    if min_value < 3553 and max_value > 9100:
+      row = {
+        'wavelength': spectrum['wavelength'].tolist(),
+        'flux_list': spectrum['flux_list'].tolist(),
+        'objid': spectrum['objid'],
+        'plate': spectrum['plate'],
+        'class': spectrum['class'],
+        'zErr': spectrum['zErr'],
+        'dec': spectrum['dec'],
+        'ra': spectrum['ra'],
+        'z': spectrum['z'],
+      }
+
+      rows_after_cutoff.append(row)
+    
+  cut_df = pd.DataFrame(rows_after_cutoff)
+
+
+  # df.loc[(np.amin(df['wavelength'].tolist()) < 3850) & 
+  #        (np.amax(df['wavelength'].tolist()) < 9100)]
+
+
+
+
+cutoff(spectra=spectra)
+
+
+def check_minmax_values(spectra=spectra, sigma=16, downsize=8):
+  min_wavelength_values = []
+  max_wavelength_values = []
+
+  for index, spectrum in islice(spectra.iterrows(), 500):
     min_wavelength_values.append(np.amin(spectrum['wavelength'].tolist()))
     max_wavelength_values.append(np.amax(spectrum['wavelength'].tolist()))
 
@@ -79,11 +131,10 @@ def process_all_objects(spectra=spectra, sigma=16, downsize=8):
     y_list=max_wavelength_values,
     x_list=range(len(max_wavelength_values)),
     title='Maximum Wavelength Values',
-    ymin = 6000,
-    ymax = 14000,
+    ymin=6000,
+    ymax=14000,
     xlabel='',
     ylabel='Wavelength',
     filename='maximum-wavelength-values'
   )
 
-# process_all_objects(spectra=spectra, sigma=8, downsize=4)
