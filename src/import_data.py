f2cbffd4e1e7af2629583c2cbe7de1e94484285d
Andrew Chepreghy <andris788@gmail.com>
2019-07-18 15:20:27 +0200

    refactor(data-import): got data import up and running, slight refactoring

--- src/import_data.py
+++ src/import_data.py
@@ -33,82 +33,100 @@ def get_save_SDSS_from_coordinates(ra, dec):
     print(length)
     number_none = 0
     while counter < int(length):
-        counter += 1
+      counter += 1
 
-        try:
+      try:
+        pos = coords.SkyCoord((ra[counter]) * u.deg, (dec[counter]) * u.deg, frame='icrs')
+        xid = SDSS.query_region(pos, spectro=True)#, radius=5 * u.arcsec)
+        #
+        #         print(xid)
+        #         print(counter)
+        #         print(type(xid))
 
-            pos = coords.SkyCoord((ra[counter]) * u.deg, (dec[counter]) * u.deg, frame='icrs')
-            xid = SDSS.query_region(pos, spectro=True)#, radius=5 * u.arcsec)
-            #
-            #         print(xid)
-            #         print(counter)
-            #         print(type(xid))
-
-            #         typ="<class 'NoneType'>"
-            # print("type:",type(xid))
-            # print(counter)
-            # print(len(xid))
-            # print(xid)
-            # print(Table(xid[0]))
-            if xid != None:
-
-                if len(xid) > 1:
-                    # print(len(xid))
-                    # print(xid[0])
-                    # print(xid)
-                    xid = Table(xid[0])
-                elif len(xid) == 1:
-                    xid = xid
-            elif xid == None:
-                # print("here")
-                number_none = 1+ number_none
-                print(counter)
-                continue
+        #         typ="<class 'NoneType'>"
+        # print("type:",type(xid))
+        # print(counter)
+        # print(len(xid))
+        # print(xid)
+        # print(Table(xid[0]))
+        if xid != None:
+          if len(xid) > 1:
+              # print(len(xid))
+              # print(xid[0])
+              # print(xid)
+              xid = Table(xid[0])
+          elif len(xid) == 1:
+            xid = xid
+        elif xid == None:
+          # print("here")
+          number_none = 1+ number_none
+          print(counter)
+          continue
 
 
 
-            # print("this is first row:", xid[0])
-            # print(type(xid))
+        # print("this is first row:", xid[0])
+        # print(type(xid))
 
-            # print(xid)
+        # print(xid)
 
-            #         print(type(xid))
-            #         print(counter)
+        #         print(type(xid))
+        #         print(counter)
 
-            sp = SDSS.get_spectra(matches=xid)
-            # print(sp)
+        sp = SDSS.get_spectra(matches=xid)
+        # print(sp)
 
-            # plate_tab = xid["plate"]
+        # plate_tab = xid["plate"]
 
-            # plate = plate_tab[0]
-            # print(plate)
-            # print("here")
-            # so = SDSS.query_specobj(plate=plate  , fields=['ra', 'dec', 'z', 'zErr'])
-            # fSpecClassN=so['fSpecClassN']
-            # soclass = SDSS.query_specobj(plate=plate , fields=['specClass'][1])
-            # sdf=pd.DataFrame(so)
-            # print(soclass)
-            # fSpecClassN= soclass['specClass'][1]
-            # print(fSpecClassN)
+        # plate = plate_tab[0]
+        # print(plate)
+        # print("here")
+        # so = SDSS.query_specobj(plate=plate  , fields=['ra', 'dec', 'z', 'zErr'])
+        # fSpecClassN=so['fSpecClassN']
+        # soclass = SDSS.query_specobj(plate=plate , fields=['specClass'][1])
+        # sdf=pd.DataFrame(so)
+        # print(soclass)
+        # fSpecClassN= soclass['specClass'][1]
+        # print(fSpecClassN)
 
 
-            df['flux_list'].append(sp[0][1].data['flux'])
-            df['wavelength'].append(10. ** sp[0][1].data['loglam'])
-            df['z'].append(xid['z'])
-            df['ra'].append(xid['ra'])
-            df['dec'].append(xid['dec'])
-            df['objid'].append(xid['objid'])
-            # df['plate'].append(plate)
-            #         df['coordinate'].append(coordinate)
-            # df['zErr'].append(so['zErr'])
-            # df['fSpecClassN'].append(so['fSpecClassN'])
+        df['flux_list'].append(sp[0][1].data['flux'])
+        df['wavelength'].append(10. ** sp[0][1].data['loglam'])
+        df['z'].append(xid['z'])
+        df['ra'].append(xid['ra'])
+        df['dec'].append(xid['dec'])
+        df['objid'].append(xid['objid'])
+        # df['plate'].append(plate)
+        # df['coordinate'].append(coordinate)
+        # df['zErr'].append(so['zErr'])
+        # df['fSpecClassN'].append(so['fSpecClassN'])
 
-            # print(df)
+        # print(df)
 
-        except:
-            print("Error")
-            n = n +1
+      except:
+        print("Error")
+        n = n + 1
+
     # print(df)
     df = pd.DataFrame(df)
     df.to_pickle('data/sdss/Nikki_35001-40000.pkl')
     print("number of None values for Xid is:", number_none)
+
+coord_list = pd.read_csv("data/lowz.csv")
+start = time.time()
+
+ra_list = coord_list["ra"].tolist()
+dec_list = coord_list["dec"].tolist()
+end = time.time()
+tt = end - start
+print("time for listing is:", tt)
+
+start1 = time.time()
+ra = ra_list[40001:45000]
+dec = dec_list[40001:45000]
+get_save_SDSS_from_coordinates(ra, dec)
+end1 = time.time()
+
+tt1 = end1- start1
+length = len(ra) - 1
+print("time for " + str(length) + " stellar objects:", tt1)
