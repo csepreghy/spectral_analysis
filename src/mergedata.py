dd07f3666b402d420b925cd5c089a6dea5c3dc67
Andrew Chepreghy <andris788@gmail.com>
2019-08-09 18:22:36 +0200

    refactor(mergedata): refactored merge data

--- src/mergedata.py
+++ src/mergedata.py
@@ -3,17 +3,23 @@ import numpy as np
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
 
 def merge_data(filenames):
-    with open('data/'+filenames[0]+'.pkl', 'rb') as g:
-                data = pickle.load(g)
-    for i in range(1,len(filenames)):
-        with open('data/'+filenames[i]+'.pkl', 'rb') as f:
-            x = pickle.load(f)
-        datalist = pd.concat([data, x], ignore_index=True)
-        data = datalist
-    return data
+  with open('data/sdss/spectra/'+filenames[0], 'rb') as g:
+    data = pickle.load(g)
 
-names = ['FinalTable_Nikki(0-10000)', 'FinalTable_10-15-Zoe', 'FinalTable_15-30_Zoe', 'FinalTable_30001-35000(Nikki)', 'FinalTable_35001-40000(Nikki)', 'FinalTable_Andrew(40-45)']
-all_data = merge_data(names)
-all_data.to_pickle("./data/alldatamerged.pkl")
+  for i in range(1,len(filenames)):
+    with open('data/sdss/spectra/' + filenames[i], 'rb') as f:
+      x = pickle.load(f)
+      datalist = pd.concat([data, x], ignore_index=True)
+      data = datalist
+
+  return data
+
+path = 'data/sdss/spectra'
+filenames = [f for f in listdir(path) if isfile(join(path, f))]
+
+all_data = merge_data(filenames)
+all_data.to_pickle("data/alldatamerged_test.pkl")
