5ebe02293a31c606ab847c20a0f56bc43498ddc4
Andrew Chepreghy <andris788@gmail.com>
2019-08-12 11:23:11 +0200

    refator(merge): minor refactor to mergedata.py

--- src/mergedata.py
+++ src/mergedata.py
@@ -7,19 +7,22 @@ from os import listdir
 from os.path import isfile, join
 
 def merge_data(filenames):
-  with open('data/sdss/spectra/'+filenames[0], 'rb') as g:
+  with open('data/sdss/spectra-meta/'+filenames[0], 'rb') as g:
     data = pickle.load(g)
 
   for i in range(1,len(filenames)):
-    with open('data/sdss/spectra/' + filenames[i], 'rb') as f:
+    with open('data/sdss/spectra-meta/' + filenames[i], 'rb') as f:
       x = pickle.load(f)
       datalist = pd.concat([data, x], ignore_index=True)
       data = datalist
 
   return data
 
-path = 'data/sdss/spectra'
+path = 'data/sdss/spectra-meta'
 filenames = [f for f in listdir(path) if isfile(join(path, f))]
 
 all_data = merge_data(filenames)
-all_data.to_pickle("data/alldatamerged_test.pkl")
+all_data.to_pickle('data/alldatamerged_test.pkl')
+
+df = pd.read_pickle('data/alldatamerged_test.pkl')
+print('df', df)
