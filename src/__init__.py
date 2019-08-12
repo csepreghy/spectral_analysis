2f2110fe7ad90f95465961cf6616b580864dec08
Andrew Chepreghy <andris788@gmail.com>
2019-08-12 15:50:11 +0200

    refactor(init): put the init file into src and restructured it

---
+++ src/__init__.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import time as time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from data_preprocessing import filter_sources, spectrum_cutoff, create_continuum, merge_lines_and_continuum
+from get_spectrallines import get_spectrallines
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.datasets.samples_generator import make_blobs
+
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.utils import to_categorical
+
+from plotify import Plotify
+
+# 1) Get coordinates from query - #
+# 2) Import data - # 
+# 3) Merge spectra with table containing other information - #
+# 4) Merge all data
+# 5) Filter Out Spectra with not enough values - #
+# 6) Get spectral lines - #
+# 7) Cut off values from the sides to have the same range for all spectra - #
+# 8) Merge spectral lines with the continuum to one table
+# 9) Run the ML algorithms - #
+
+
+df_spectra = pd.read_pickle('data/sdss/spectra-meta/spectra-meta-merged_5001-10000.pkl')
+df_filtered = filter_sources(df = df_spectra)
+
+print('DF Filtered: ')
+print(df_filtered.columns)
+print(df_filtered.head())
+
+df_spectral_lines = get_spectrallines(df_filtered)
+print('Spectral Lines')
+print(df_spectral_lines.head())
+
+df_spectral_lines.to_pickle('spectral_lines_df_5001-10000.pkl')
+# df_spectral_lines = pd.read_pickle('spectral_lines_df_5001-10000.pkl')
+
+df_cutoff = spectrum_cutoff(df = df_filtered)
+print('DF Cutoff: ')
+print(df_cutoff.columns)
+print(df_cutoff)
+
+df_continuum = create_continuum(df = df_cutoff, sigma=8, downsize=8)
+df_continuum.to_pickle('data/continuum_df_5001-10000.pkl')
+# df_continuum = pd.read_pickle('data/continuum_df_5001-10000.pkl')
+print('DF Continuun: ')
+print(df_continuum.columns)
+print(df_continuum)
+
+df_preprocessed = merge_lines_and_continuum(df_spectral_lines, df_continuum)
+df_preprocessed.to_pickle('data/preprocessed_5001-10000.pkl')
+#df_preprocessed = pd.read_pickle('data/preprocessed_5001-10000.pkl')
+
+print('DF Preprocessed (Final)')
+print(df_preprocessed.columns)
+print(df_preprocessed.head())
+
+# model = create_model(df_preprocessed, configs['neural_network'])
+
+# df_continuum = pd.read_pickle('continuum_df.pkl')
+# df_spectral_lines = pd.read_pickle('spectral_lines_df.pkl')
+
+# spectra = pd.read_pickle('data/alldatamerged.pkl')
+# df_filtered = filter_sources(df=spectra)
+# print('DF Filtered: ')
+# print(df_filtered.head())
+
+# """
+# df_spectral_lines = get_spectrallines(df_filtered)
+# print('Spectral Lines')
+# print(df_spectral_lines.head())
+# df_spectral_lines.to_pickle('spectral_lines2_df.pkl')
+# """
+# df_cutoff = spectrum_cutoff(df=df_filtered)
+# df_continuum = create_continuum(df=df_cutoff, sigma=16, downsize=8)
+# df_continuum.to_pickle('continuum_df.pkl')
+
+
+# df_complete = merge_lines_and_continuum(df_spectral_lines, df_continuum)
+# print("DF Complete: ")
+# print(df_complete.head())
+# df_complete.to_pickle('COMPLETE_df.pkl')
+
