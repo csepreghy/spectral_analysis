c411054ec4da3dc1154a4b6d4b6bc48103bf9aa3
Andrew Chepreghy <andris788@gmail.com>
2019-08-15 15:14:22 +0200

    feat(nn): combining cnn and mlp works

--- src/neural_network/neural_network_classifier.py
+++ src/neural_network/neural_network_classifier.py
@@ -13,10 +13,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.callbacks import History
-from tensorflow.keras.layers import Dense, Dropout, Activation, GlobalAveragePooling1D, Flatten, Conv1D, MaxPooling1D, MaxPooling2D, Input
+from tensorflow.keras.layers import Dense, Dropout, Activation, GlobalAveragePooling1D, Flatten, Conv1D, MaxPooling1D, MaxPooling2D, Input, concatenate
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.utils import to_categorical
+
+from .models import create_cnn, create_mlp
 
 
 # This is a simple feed forward neural network that uses Keras and Tensorflow.
@@ -118,19 +119,6 @@ def run_neural_network(df, config):
   X_test_spectra = scaler.transform(X_test_spectra)
   X_val_spectra = scaler.transform(X_val_spectra)
 
-  n_classes = 3
-
-  # model_m = Sequential()
-  # model_m.add(Conv1D(100, 10, activation='relu', input_shape=(X_train_spectra.shape[1], 1)))
-  # model_m.add(Conv1D(100, 10, activation='relu'))
-  # model_m.add(MaxPooling1D(1))
-  # model_m.add(Conv1D(160, 10, activation='relu'))
-  # model_m.add(Conv1D(160, 10, activation='relu'))
-  # model_m.add(GlobalAveragePooling1D())
-  # model_m.add(Dropout(0.5))
-  # model_m.add(Dense(num_classes, activation='softmax'))
-  # print(model_m.summary())
-  # model_m.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
   print('X_test_spectra.shape', type(X_test_spectra))
 
@@ -143,78 +131,39 @@ def run_neural_network(df, config):
   print('y_train.shape', y_train.shape)
   print('y_test.shape', type(y_test))
 
-  model_m = Sequential()
-  model_m.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(X_train_spectra.shape[1], 1)))
-  model_m.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
-  model_m.add(Dropout(0.5))
-  model_m.add(MaxPooling1D(pool_size=2))
-  model_m.add(Flatten())
-  model_m.add(Dense(100, activation='relu'))
-  model_m.add(Dense(n_classes, activation='softmax'))
-  model_m.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-  history = model_m.fit(X_train_spectra, y_train, validation_data=(X_test_spectra, y_test), epochs=60, verbose=0)
-
-  # history = model_m.fit(X_train_spectra,
-  #                       y_train,
-  #                       batch_size=12,
-  #                       epochs=20,
-  #                       callbacks=callbacks_list,
-  #                       verbose=1)
-
-  # # define model
-  # model = Sequential()
-  # # model.add(Dense(256, input_dim=X_train_source_info.shape[1], activation='relu', kernel_initializer='he_uniform'))
-  # # model.add(Dense(256, input_dim=256, activation='relu', kernel_initializer='he_uniform'))
-  # # model.add(Dense(256, input_dim=256, activation='relu', kernel_initializer='he_uniform'))
-  # # model.add(Dense(3, activation='softmax'))
-
-  # # # define two sets of inputs
-  # input_source_info = Input(shape=(X_train_source_info.shape[1],))
-  # input_spectra = Input(shape=(X_train_spectra.shape[1],))
+  cnn = create_cnn(input_length=X_train_spectra.shape[1])
+  mlp = create_mlp(input_shape=X_train_source_info.shape[1])
   
-  # # # the first branch operates on the first input
-  # model_source_info = Dense(8, activation="relu")(input_source_info)
-  # model_source_info = Dense(4, activation="relu")(model_source_info)
-  # model_source_info = Model(inputs=input_source_info, outputs=model_source_info)
+  # we only complibe the combined outputs
+  # cnn.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-  # print('model_source_info', model_source_info)
+  callbacks_list = [
+    # for saving the best model
+    # keras.callbacks.ModelCheckpoint(
+    #     filepath='best_model.{epoch:02d}-{val_loss:.2f}.h5',
+    #     monitor='val_loss', save_best_only=True),
+    keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=3)
+  ]
+
+
+  # combine the output of the two branches
+  combined = concatenate([cnn.output, mlp.output])
+
+  # apply a FC layer and then a regression prediction on the combined outputs
+  final_classifier = Dense(128, activation="relu")(combined)
+  final_classifier = Dense(3, activation="softmax")(final_classifier)
   
-  # # filters for the CNN
-  # filters = (8, 16, 32)
+  model = Model(inputs=[mlp.input, cnn.input], outputs=final_classifier)
 
-  # for (i, f) in enumerate(filters):
-  #   # if this is the first CONV layer then set the input
-  #   # appropriately
-  #   if i == 0:
-  #     model_spectra = input_spectra
+  opt = SGD(lr=0.01, momentum=0.9)
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-  #   # CONV => RELU => BN => POOL
-  #   model_spectra = Conv1D(f, (3), padding="same")(model_spectra)
-  #   model_spectra = Activation("relu")(model_spectra)
-  #   # model_spectra = BatchNormalization(axis=chanDim)(model_spectra)
-  #   model_spectra = MaxPooling2D(pool_size=(2, 2))(model_spectra)
-
-  # # the second branch opreates on the second input
-  # model_spectra = Dense(64, activation="relu")(input_spectra)
-  # model_spectra = Dense(32, activation="relu")(model_spectra)
-  # model_spectra = Dense(4, activation="relu")(model_spectra)
-  # model_spectra = Model(inputs=input_spectra, outputs=model_spectra)
-
-  # # flatten the volume, then FC => RELU => BN => DROPOUT
-  # model_spectra = Flatten()(model_spectra)
-  # model_spectra = Dense(16)(model_spectra)
-  # model_spectra = Activation("relu")(model_spectra)
-  # # model_spectra = BatchNormalization(axis=chanDim)(model_spectra)
-  # model_spectra = Dropout(0.5)(model_spectra)
-
-  # # apply another FC layer, this one to match the number of nodes
-  # # coming out of the MLP
-  # model_spectra = Dense(4)(model_spectra)
-  # model_spectra = Activation("relu")(model_spectra)
-
-  # # construct the CNN
-  # model = Model(inputs, model_spectra)
+  print("[INFO] training model...")
+  history = model.fit(x=[X_train_source_info, X_train_spectra],
+                      y=y_train,
+                      validation_data=([X_test_source_info, X_test_spectra], y_test),
+                      epochs=50,
+                      callbacks=callbacks_list)
 
   # # combine the output of the two branches
   # combined_input = concatenate([model_source_info.output, model_spectra.output])
@@ -241,8 +190,8 @@ def run_neural_network(df, config):
 
 
   # evaluate the model
-  _, train_acc = model_m.evaluate(X_train_spectra, y_train, verbose=0)
-  _, test_acc = model_m.evaluate(X_test_spectra, y_test, verbose=0)
+  _, train_acc = model.evaluate([X_train_source_info, X_train_spectra], y_train, verbose=0)
+  _, test_acc = model.evaluate([X_test_source_info, X_test_spectra], y_test, verbose=0)
   print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
   # plot loss during training
   plt.subplot(211)
@@ -259,7 +208,7 @@ def run_neural_network(df, config):
   plt.legend()
   plt.show()
 
-  return model_m
+  return cnn
 
 
 def train_test_split(X, y, test_size):
@@ -270,6 +219,8 @@ def train_test_split(X, y, test_size):
   
   X_test = X[-n_test:]
   X_train = X[:n_train]
+
+  print('len(X_train', len(X_train))
 
   # print('type(X_test', type(X_test))
 
@@ -282,6 +233,12 @@ def train_test_split(X, y, test_size):
   
   else:
     return X_train, X_test
+  
+def summarize_results():
+  print('hello')
+
+def evaluate_model():
+  print('hello')
 
 
 # def run_cnn(df, config)
