ad2eb1f93e4024c3bf58ed081ee65622cbe5621d
Andrew <andris788@gmail.com>
2020-01-09 15:13:29 +0100

    refactor: added __main__, started refactoring with docstring

--- src/__init__.py
+++ src/__init__.py
@@ -29,44 +29,60 @@ from plotify import Plotify
 
 
 
-df_spectra = pd.read_pickle('data/spectra-meta-0-70k.pkl')
-
+df_spectra = pd.read_pickle('data/sdss/spectra-meta/spectra-meta-merged_10001-20000.pkl')
+print(f'df_spectra = {df_spectra}')
 
 # df_filtered = filter_sources(df = df_spectra)
 # df_filtered.to_pickle('data/spectra-meta-filtered_0-70k.pkl')
 # df_filtered.to_msgpack('data/spectra-meta-filtered_0-70k.msg')
-df_filtered = pd.read_msgpack('data/spectra-meta-filtered_0-70k.msg')
+# df_filtered = pd.read_msgpack('data/spectra-meta-filtered_0-70k.msg')
 
-print('DF Filtered: ')
-print(df_filtered.columns)
-print(df_filtered.head())
+# print('DF Filtered: ')
+# print(df_filtered.columns)
+# print(df_filtered.head())
 
-df_spectral_lines = get_spectrallines(df_filtered)
-print('Spectral Lines')
-print(df_spectral_lines.head())
+# df_spectral_lines = get_spectrallines(df_filtered)
+# print('Spectral Lines')
+# print(df_spectral_lines.head())
 
-df_spectral_lines.to_pickle('data/spectral_lines_df_0-70k.pkl')
-# # df_spectral_lines = pd.read_pickle('spectral_lines_df_5001-10000.pkl')
+# df_spectral_lines.to_pickle('data/spectral_lines_df_0-70k.pkl')
+# #s df_spectral_lines = pd.read_pickle('spectral_lines_df_5001-10000.pkl')
 
-df_cutoff = spectrum_cutoff(df = df_filtered)
-print('DF Cutoff: ')
-print(df_cutoff.columns)
-print(df_cutoff)
+# df_cutoff = spectrum_cutoff(df = df_filtered)
+# print('DF Cutoff: ')
+# print(df_cutoff.columns)
+# print(df_cutoff)
 
-df_continuum = create_continuum(df = df_cutoff, sigma=8, downsize=8)
-df_continuum.to_pickle('data/continuum_df_0-70k.pkl')
-# df_continuum = pd.read_pickle('data/continuum_df_5001-10000.pkl')
-print('DF Continuun: ')
-print(df_continuum.columns)
-print(df_continuum)
+def create_df_continuum():
+	"""
+	create_df_continuum()
 
-df_preprocessed = merge_lines_and_continuum(df_spectral_lines, df_continuum)
-df_preprocessed.to_pickle('data/preprocessed_0-70k.pkl')
-# df_preprocessed = pd.read_pickle('data/preprocessed_5001-10000.pkl')
+	A wrapper for craete_continuum in order to better structure the __init__.py
+	
+	Makes Use Of
+	------------
+	create_continuum() --imported from src/data_preprocessing.py
+	
+	Returns
+	-------
+	out: DataFrame()
+		A pandas dataframe that has the flux_list which went through Gaussian smoothing
+		and a downsizing with the factor tunable here.
+	"""
+	df_continuum = create_continuum(df = df_cutoff, sigma=8, downsize=8)
+	df_continuum.to_pickle('data/continuum_df_0-70k.pkl')
+	# df_continuum = pd.read_pickle('data/continuum_df_5001-10000.pkl')
+	print('DF Continuun: ')
+	print(df_continuum.columns)
+	print(df_continuum)
 
-print('DF Preprocessed (Final)')
-print(df_preprocessed.columns)
-print(df_preprocessed.head())
+# df_preprocessed = merge_lines_and_continuum(df_spectral_lines, df_continuum)
+# df_preprocessed.to_pickle('data/preprocessed_0-70k.pkl')
+# # df_preprocessed = pd.read_pickle('data/preprocessed_5001-10000.pkl')
+
+# print('DF Preprocessed (Final)')
+# print(df_preprocessed.columns)
+# print(df_preprocessed.head())
 
 # model = create_model(df_preprocessed, configs['neural_network'])
 
@@ -94,3 +110,10 @@ print(df_preprocessed.head())
 # print(df_complete.head())
 # df_complete.to_pickle('COMPLETE_df.pkl')
 
+
+def main():
+	create_df_continuum()
+
+
+if __name__ == '__main__':
+	main()
