837137a878edd0d58c7d7826c3352769441ea647
Andrew <andris788@gmail.com>
2020-01-22 22:27:53 +0100

    fixed imports from old versions

--- src/ml.py
+++ src/ml.py
@@ -2,35 +2,35 @@ import pandas as pd
 import sys
 import pickle
 
-from neural_network.neural_network_classifier import run_neural_network
-from gaussian_classifier import run_gaussian_clf
+from classifiers.neural_network.neural_network_classifier import run_neural_network
+from classifiers.gaussian_classifier import run_gaussian_clf
 
 def create_model(df, config):
-#  if config['type'] == 'xgboost': model = run_xgboost(df, config)
-  if config['type'] == 'neural_network': model = run_neural_network(df, config)
-  if config['type'] == 'gaussian_clf': model = run_gaussian_clf(df, config)
+	#  if config['type'] == 'xgboost': model = run_xgboost(df, config)
+	if config['type'] == 'neural_network': model = run_neural_network(df, config)
+	if config['type'] == 'gaussian_clf': model = run_gaussian_clf(df, config)
 
-  return model
+	return model
 
 configs = {
-  'neural_network': {
-    'type': 'neural_network',
-    'batch_size': 5,
-    'hidden_layers': [128, 128],
-    'n_epochs': 5
-  },
-  'xgboost': {
-    'type': 'xgboost',
-    'max_depth': 3,
-    'learning_rate': 0.1
-  },
-  'gaussian_clf': {
-    'type' : 'gaussian_clf',
-    'kernel_val' : 1.0
-  }
+	'neural_network': {
+		'type': 'neural_network',
+		'batch_size': 5,
+		'hidden_layers': [128, 128],
+		'n_epochs': 5
+	},
+	'xgboost': {
+		'type': 'xgboost',
+		'max_depth': 3,
+		'learning_rate': 0.1
+	},
+	'gaussian_clf': {
+		'type' : 'gaussian_clf',
+		'kernel_val' : 1.0
+	}
 }
 
-df_preprocessed = pd.read_pickle('data/sdss/spectra_10001-20000.pkl')
+df_preprocessed = pd.read_pickle('data/sdss/spectra-meta/spectra-meta-0-70k.pkl')
 
 print('df', df_preprocessed)
 model = create_model(df_preprocessed, configs['neural_network'])
