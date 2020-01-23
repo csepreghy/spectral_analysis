726b85834baf9b53cc67790f6efa6f71b6d7b0db
Andrew <andris788@gmail.com>
2020-01-23 10:59:50 +0100

    made changes to the download mechanism in how it handles duplicates

--- src/merge_tables.py
+++ src/merge_tables.py
@@ -3,23 +3,27 @@ import pandas as pd
 import time as time
 
 
-def merge_with_metatable(from_sp, to_sp, save=False):
+def merge_with_metatable(from_sp, to_sp, save=False, df=None):
 	"""
 	merge_with_metatable()
 
 	Parameters
 	----------
 	from_sp : string
-		The number from which to merge spectra with meta-data. String, beceause it must match
-		the filename in folder data/sdss/spectra/
+		The number from which to merge spectra with meta-data. String, beceause it 
+		must match the filename in folder data/sdss/spectra/
 	
 	to_sp : string
-		The number which specifies the upper limit to merge spectra with meta-data. String, 
-		beceause it must match the filename in folder data/sdss/spectra/
+		The number which specifies the upper limit to merge spectra with meta-data. 
+		String, beceause it must match the filename in folder data/sdss/spectra/
 
 	save : boolean
 		When True, save the resulting merged table into a pickle
 		When False, don't save the resulting merged table
+	
+	df : pd.DataFrame
+		The DataFrame that comes from downloading the raw spectral data. None by
+		default, in which case its loaded from disk.
 	
 	Returns
 	-------
@@ -52,14 +56,24 @@ def merge_with_metatable(from_sp, to_sp, save=False):
 					'ra'
 	"""
 
-	df_spectra = pd.read_pickle('data/sdss/spectra/spectra_' + from_sp + '-' + to_sp + '.pkl')
+	if df == None:
+		df_spectra = pd.read_pickle('data/sdss/spectra/spectra_' + from_sp + '-' + to_sp + '.pkl')
+	
+	else:
+		df_spectra = df
+
+
 	df_meta_data = pd.read_pickle('data/sdss/meta_table.pkl')
 
 	df_meta_data["objid"] = df_meta_data['bestObjID'].astype(int)
 	df_spectra['objid'] = df_spectra['objid'].astype(int)
 	
-	df_spectra.drop_duplicates('objid')
-	df_meta_data.drop_duplicates()
+	print(f'df_spectra before removing duplicates = {df_spectra.shape[0]}')
+
+	df_spectra = df_spectra.drop_duplicates(subset=['objid'])
+	df_meta_data = df_meta_data.drop_duplicates(subset=['objid', 'z'])
+
+	print(f'df_spectra after removing duplicates = {df_spectra.shape[0]}')
 	
 	df_meta_data = df_meta_data.drop(columns={"specObjID"})
 
@@ -83,12 +97,16 @@ def main():
 	"""
 	print('merge_tables.py -- __main__')
 
-	from_sp = '10001'
-	to_sp = '20000'
+	# from_sp = '10001'
+	# to_sp = '20000'
 
-	merge_with_metatable(from_sp, to_sp)
+	# merge_with_metatable(from_sp, to_sp)
 
-	df = pd.read_pickle('data/sdss/spectra-meta/spectra-meta-merged_' + from_sp + '-' + to_sp + '.pkl')
+	# df = pd.read_pickle('data/sdss/spectra-meta/spectra-meta-merged_' + from_sp + '-' + to_sp + '.pkl')
+	df_raw = pd.read_pickle('data/sdss/spectra/spectra_0-5000.pkl')
+	print(f'df_raw = {df_raw}')
+	df_meta = pd.read_pickle('data/sdss/spectra-meta/spectra-meta_0-5000.pkl')
+	print(f'df_meta = {df_meta}')
 
 if __name__ == '__main__':
   	main()
