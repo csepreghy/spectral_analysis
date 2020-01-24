565fc26a2b5a2ce48ebe544731f1cdaea239aa89
Andrew <andris788@gmail.com>
2020-01-24 10:48:07 +0100

    added progresshar and refactored merging data

--- src/mergedata.py
+++ src/mergedata.py
@@ -5,24 +5,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
+from tqdm.auto import tqdm
 
 def merge_data(filenames):
-  with open('data/sdss/spectra-meta/' + filenames[0], 'rb') as g:
-    data = pickle.load(g)
+	with open('data/sdss/spectra-meta/0-50/' + filenames[0], 'rb') as g:
+		data = pickle.load(g)
 
-  for i in range(1,len(filenames)):
-    with open('data/sdss/spectra-meta/' + filenames[i], 'rb') as f:
-      x = pickle.load(f)
-      datalist = pd.concat([data, x], ignore_index=True)
-      data = datalist
+	for i in tqdm(range(len(filenames))):
+		with open('data/sdss/spectra-meta/0-50/' + filenames[i], 'rb') as f:
+			x = pickle.load(f)
+			datalist = pd.concat([data, x], ignore_index=True)
+			data = datalist
 
-  return data
+	return data
 
-path = 'data/sdss/spectra-meta'
-filenames = [f for f in listdir(path) if isfile(join(path, f))]
+def main():
+	# path = 'data/sdss/spectra-meta/0-50'
+	# filenames = [f for f in listdir(path) if isfile(join(path, f))]
+	# try: filenames.remove('.DS_Store')
+	# except: print('.DS_Store is not in folder')
+	
+	# print(f'filenames = {filenames}')
 
-all_data = merge_data(filenames)
-all_data.to_pickle('data/alldatamerged_test.pkl')
 
-df = pd.read_pickle('data/alldatamerged_test.pkl')
-print('df', df)
+	# all_data = merge_data(filenames)
+	# print(f'all_data = {all_data}')
+	# all_data.to_parquet('data/sdss/0-50_merged.parquet')
+
+	df = pd.read_parquet('data/sdss/spectra-meta/0-50_merged.parquet')
+	print('df parquet', df)
+
+if __name__ == "__main__":
+	main()
