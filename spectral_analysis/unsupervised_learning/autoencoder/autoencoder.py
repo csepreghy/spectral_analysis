6088615fa40242396af1157618cf5b35f6efd1b1
Andrew <andris788@gmail.com>
2020-02-23 13:05:41 +0100

    implemented keras-tuner on input layer

--- spectral_analysis/unsupervised_learning/autoencoder/autoencoder.py
+++ spectral_analysis/unsupervised_learning/autoencoder/autoencoder.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import time
 
 from tensorflow.keras.layers import Input, Dense, Flatten, Conv1D, MaxPooling1D, UpSampling1D, BatchNormalization, Reshape
 from tensorflow.keras.models import Model, Sequential
@@ -9,7 +10,12 @@ from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 
+from kerastuner.engine.hyperparameters import HyperParameters
+from kerastuner.tuners import RandomSearch
+
 from spectral_analysis.data_preprocessing.data_preprocessing import remove_bytes_from_class, plot_spectrum, get_wavelengths_from_h5
+
+
 
 class AutoEncoder():
     def __init__(self, df_source_info, df_fluxes):
@@ -22,8 +28,7 @@ class AutoEncoder():
 
         # print(f'self.X = {self.X}')
 
-        self.autoencoder_model = self.build_model()
-        self.autoencoder_model.compile(loss='mse', optimizer='nadam')
+        # self.autoencoder_model = self.build_model()
     
     def _prepare_data(self, df_source_info, df_fluxes):
         if "b'" in df_source_info['class'][0]:
@@ -47,11 +52,15 @@ class AutoEncoder():
         # plot_spectrum(X[0], wavelengths)
         return X
     
-    def build_model(self):
+    def build_model(self, hp):
         input_layer = Input(shape=(self.X.shape[1], 1))
 
         # encoder
-        x= Conv1D(128, 8, activation='relu', padding='same')(input_layer)
+        x = Conv1D(filters=hp.Choice('encoder_conv_input_layer_filters', values=[16, 32, 64, 128, 256, 512]), 
+                   kernel_size=hp.Choice('encoder_conv_input_layer_kernel_size', values=[2, 3, 4, 8, 16, 32, 64]),
+                   activation='relu', 
+                   padding='same')(input_layer)
+        
         x = MaxPooling1D(2, padding="same")(x)
         x = Conv1D(64, 8, activation="relu", padding="same")(x)
         x = MaxPooling1D(2, padding='same')(x)
@@ -72,28 +81,43 @@ class AutoEncoder():
         autoencoder = Model(input_layer, decoded)
         autoencoder.summary()
 
+        autoencoder.compile(loss='mse', optimizer='nadam')
+        
         return autoencoder
     
-    def train_model(self, epochs, batch_size=20):
-        early_stopping = EarlyStopping(monitor='val_loss',
-                                       min_delta=0,
-                                       patience=50,
-                                       verbose=1, 
-                                       mode='auto')
+    def train_model(self, epochs, batch_size=32):
+        tuner = RandomSearch(self.build_model,
+                             objective='val_loss',
+                             max_trials=100,
+                             executions_per_trial=1,
+                             directory='logs/keras-tuner-logs')
+    
+        tuner.search(x=self.X,
+                     y=self.X,
+                     epochs=20,
+                     batch_size=32,
+                     validation_data=(self.X, self.X))
 
-        history = self.autoencoder_model.fit(self.X, self.X,
-                                             batch_size=batch_size,
-                                             epochs=epochs,
-                                             validation_data=(self.X, self.X),
-                                             callbacks=[early_stopping])
+
+        # early_stopping = EarlyStopping(monitor='val_loss',
+        #                                min_delta=0,
+        #                                patience=50,
+        #                                verbose=1, 
+        #                                mode='auto')
+
+        # history = self.autoencoder_model.fit(self.X, self.X,
+        #                                      batch_size=batch_size,
+        #                                      epochs=epochs,
+        #                                      validation_data=(self.X, self.X),
+        #                                      callbacks=[early_stopping])
         
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
-        plt.title('Model loss')
-        plt.ylabel('Loss')
-        plt.xlabel('Epoch')
-        plt.legend(['Train', 'Test'], loc='upper left')
-        plt.show()
+        # plt.plot(history.history['loss'])
+        # plt.plot(history.history['val_loss'])
+        # plt.title('Model loss')
+        # plt.ylabel('Loss')
+        # plt.xlabel('Epoch')
+        # plt.legend(['Train', 'Test'], loc='upper left')
+        # plt.show()
     
     def eval_model(self, x_test):
         preds = self.autoencoder_model.predict(x_test)
@@ -102,10 +126,6 @@ class AutoEncoder():
 def main():
     df_fluxes = pd.read_hdf('data/sdss/preprocessed/0-50_gaussian.h5', key='fluxes')
     df_source_info = pd.read_hdf('data/sdss/preprocessed/0-50_gaussian.h5', key='spectral_data')
-
-    # autoencoder = AutoEncoder(df_source_info=df_source_info, df_fluxes=df_fluxes, code_dimension=10)
-    # autoencoder.encoder_decoder()
-    # autoencoder.fit(batch_size=32, epochs=100)
 
     ae = AutoEncoder(df_source_info, df_fluxes)
     ae.train_model(epochs=200, batch_size=32)
