44ef67e0c08653368213472d11439a9abd026ed4
Andrew <andris788@gmail.com>
2020-04-29 09:38:15 +0200

    (data-preprocessing): removed bytes from classes

--- spectral_analysis/classifiers/neural_network/mixed_input.py
+++ spectral_analysis/classifiers/neural_network/mixed_input.py
@@ -28,8 +28,9 @@ from spectral_analysis.plotify import Plotify
 from spectral_analysis.classifiers.neural_network.helper_functions import train_test_split, evaluate_model, unison_shuffled_copies
 
 class MixedInputModel():
-    def __init__(self, mainclass=None):
+    def __init__(self, mainclass=None, spectral_lines=False):
         self.mainclass = mainclass
+        self.spectral_lines = spectral_lines
 
     def _prepare_data(self, df_source_info, df_fluxes):
         columns = []
@@ -63,16 +64,17 @@ class MixedInputModel():
 
         for _, spectrum in tqdm(df_source_info[columns].iterrows(), total=len(df_source_info), desc="Preparing Data: "):
             X_row = []
-
-            for i in range(14):
-                column = f'spectral_line_{i}'
-                spectral_line = spectrum[column]
-                
-                if np.isnan(spectral_line):
-                    X_row.append(-99)
-                
-                else:
-                    X_row.append(spectral_line)
+            
+            if self.spectral_lines:
+              for i in range(14):
+                  column = f'spectral_line_{i}'
+                  spectral_line = spectrum[column]
+                  
+                  if np.isnan(spectral_line):
+                      X_row.append(-99)
+                  
+                  else:
+                      X_row.append(spectral_line)
 
             X_row.append(spectrum['z'])
             X_row.append(spectrum['zErr'])
@@ -210,9 +212,9 @@ class MixedInputModel():
         return model
 
 def main():
-    df_fluxes = pd.read_hdf('data/sdss/preprocessed/balanced_spectral_lines.h5', key='fluxes').head(75000)
-    df_source_info = pd.read_hdf('data/sdss/preprocessed/balanced_spectral_lines.h5', key='spectral_data').head(75000)
-    df_wavelengths = pd.read_hdf('data/sdss/preprocessed/balanced_spectral_lines.h5', key='wavelengths')
+    df_fluxes = pd.read_hdf('data/sdss/preprocessed/balanced.h5', key='fluxes')
+    df_source_info = pd.read_hdf('data/sdss/preprocessed/balanced.h5', key='spectral_data')
+    df_wavelengths = pd.read_hdf('data/sdss/preprocessed/balanced.h5', key='wavelengths')
 
 
     print(f'len(df_fluxes1) = {df_fluxes}')
