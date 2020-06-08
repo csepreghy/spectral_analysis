7d450e8c7c66c21e8a07c4e6bbe7fad5afdc8b0f
Andrew <andris788@gmail.com>
2020-06-08 17:00:50 +0200

    (mixed-input): added comma back

--- spectral_analysis/classifiers/neural_network/mixed_input.py
+++ spectral_analysis/classifiers/neural_network/mixed_input.py
@@ -266,7 +266,7 @@ class MixedInputModel():
                                 y=y_train,
                                 validation_data=([X_test_source_info_std, X_test_fluxes_std], y_test),
                                 epochs=self.epochs,
-                                batch_size=32)
+                                batch_size=32,
                                 callbacks=callbacks_list)
 
             # evaluate the model
