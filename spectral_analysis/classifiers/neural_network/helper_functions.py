533b9e71d343f5956c3d5f3d5fb3cfdce01ae6b7
Andrew <andris788@gmail.com>
2020-06-09 17:11:02 +0200

    (path): fixed paths

--- spectral_analysis/classifiers/neural_network/helper_functions.py
+++ spectral_analysis/classifiers/neural_network/helper_functions.py
@@ -22,8 +22,8 @@ from tensorflow.keras.layers import Dense, Activation, Input, concatenate
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.utils import to_categorical
 
-from spectral_analysis.plotify import Plotify
-from spectral_analysis.data_preprocessing.bpt_diagram import plot_bpt_diagram
+from spectral_analysis.spectral_analysis.plotify import Plotify
+from spectral_analysis.spectral_analysis.data_preprocessing.bpt_diagram import plot_bpt_diagram
 
 
 
@@ -254,28 +254,6 @@ def main():
     fig.tight_layout()
 
     plt.title('Mixed-input NN Training on 51,200 sources')
-    plt.show()
-
-    t = np.arange(0.01, 10.0, 0.01)
-    data1 = np.exp(t)
-    data2 = np.sin(2 * np.pi * t)
-
-    fig, ax1 = plt.subplots()
-
-    color = 'tab:red'
-    ax1.set_xlabel('time (s)')
-    ax1.set_ylabel('exp', color=color)
-    ax1.plot(t, data1, color=color)
-    ax1.tick_params(axis='y', labelcolor=color)
-
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-
-    color = 'tab:blue'
-    ax2.set_ylabel('sin', color=color)  # we already handled the x-label with ax1
-    ax2.plot(t, data2, color=color)
-    ax2.tick_params(axis='y', labelcolor=color)
-
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
     plt.show()
 
 if __name__ == "__main__":
