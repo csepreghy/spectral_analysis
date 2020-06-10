2894ca193f894242bd53a8496bfde144fb95ae7d
Andrew <andris788@gmail.com>
2020-06-10 14:30:52 +0200

    (cnn): hopefully fixed weird syntax issue

--- spectral_analysis/classifiers/neural_network/best_cnn.py
+++ spectral_analysis/classifiers/neural_network/best_cnn.py
@@ -18,101 +18,77 @@ import time
 LOG_DIR = f"{int(time.time())}"
 
 class CNN:
-    def __init__(self, df_fluxes, epochs):
-        self.input_length = len(df_fluxes.columns) - 1
-        self.epochs = epochs
+	def __init__(self, df_fluxes, epochs):
+		self.input_length = len(df_fluxes.columns) - 1
+		self.epochs = epochs
 
-    def _prepare_data(self, df_source_info, df_fluxes):
-        columns = []
+	def _prepare_data(self, df_source_info, df_fluxes):
+		columns = []
 
-        df_source_info['class'] = pd.Categorical(df_source_info['class'])
-        df_dummies = pd.get_dummies(df_source_info['class'], prefix='category')
-        df_dummies.columns = ['category_GALAXY', 'category_QSO', 'category_STAR']
-        df_source_info = pd.concat([df_source_info, df_dummies], axis=1)
+		df_source_info['class'] = pd.Categorical(df_source_info['class'])
+		df_dummies = pd.get_dummies(df_source_info['class'], prefix='category')
+		df_dummies.columns = ['category_GALAXY', 'category_QSO', 'category_STAR']
+		df_source_info = pd.concat([df_source_info, df_dummies], axis=1)
 
-        for column in df_source_info.columns:
-            if column not in ['class', 'dec', 'ra', 'plate', 'wavelength', 'objid', 'subClass']:
-                columns.append(column)
+		for column in df_source_info.columns:
+			if column not in ['class', 'dec', 'ra', 'plate', 'wavelength', 'objid', 'subClass']:
+				columns.append(column)
 
-        X = np.delete(df_fluxes.values, 0, axis=1)
-        y = []
+		X = np.delete(df_fluxes.values, 0, axis=1)
+		y = []
 
-        print(f'df_source_info = {df_source_info}')
+		print(f'df_source_info = {df_source_info}')
 
-        for _, spectrum in df_source_info[columns].iterrows():
-            category_GALAXY = spectrum["category_GALAXY"]
-            category_QSO = spectrum["category_QSO"]
-            category_STAR = spectrum["category_STAR"]
+		for _, spectrum in df_source_info[columns].iterrows():
+			category_GALAXY = spectrum["category_GALAXY"]
+			category_QSO = spectrum["category_QSO"]
+			category_STAR = spectrum["category_STAR"]
 
-            y_row = [category_GALAXY, category_QSO, category_STAR]
+			y_row = [category_GALAXY, category_QSO, category_STAR]
 
-            y.append(y_row)
+			y.append(y_row)
 
-        return X, y
+		return X, y
 
 
-    def _fit(self, X_train, y_train, X_test, y_test, X_val, y_val):
-        model = self._build_model()
-        print(model.summary())
+	def _fit(self, X_train, y_train, X_test, y_test, X_val, y_val):
+		model = self._build_model()
+		print(model.summary())
 
-        history = model.fit(x=X_train,
-                            y=y_train,
-                            epochs=self.epochs,
-                            batch_size=32,
-                            validation_data=(X_val, y_val),
-                            callbacks=[EarlyStopping('val_accuracy', patience=10)])
+		history = model.fit(x=X_train,
+							y=y_train,
+							epochs=self.epochs,
+							batch_size=32,
+							validation_data=(X_val, y_val),
+							callbacks=[EarlyStopping('val_accuracy', patience=10)])
 
-        # Evaluate Best Model #
-        _, train_acc = model.evaluate(X_train, y_train, verbose=0)
-        _, test_acc = model.evaluate(X_test, y_test, verbose=0)
-        print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
+		# Evaluate Best Model #
+		_, train_acc = model.evaluate(X_train, y_train, verbose=0)
+		_, test_acc = model.evaluate(X_test, y_test, verbose=0)
+		print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
 
-    def _build_model(self):
-        model = Sequential()
 
-        model.add(Conv1D(filters=64,
-                         kernel_size=3,
-                         activation='relu',
-                         input_shape=(self.input_length, 1))
+	def run(self, df_source_info, df_fluxes):
+		X, y = self._prepare_data(df_source_info, df_fluxes)
 
-        for i in range(2):
-            model.add(Conv1D(filters=128, kernel_size=3, activation='relu'))
+		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+		X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
 
-            model.add(MaxPooling1D(pool_size=2))
-        
-        model.add(Flatten())
+		scaler = StandardScaler()
 
-        for i in range(2):
-            model.add(Dense(128, activation='relu'))
+		X_train = scaler.fit_transform(X_train)
+		X_test = scaler.transform(X_test)
+		X_val = scaler.transform(X_val)
 
-        model.add(Dense(3, activation='softmax'))
-        model.compile(loss='categorical_crossentropy',
-                      optimizer='adam',
-                      metrics=['accuracy'])
+		X_train = np.expand_dims(X_train, axis=2)
+		X_test = np.expand_dims(X_test, axis=2)
+		X_val = np.expand_dims(X_val, axis=2)
 
-        return model
+		y_train = np.array(y_train)
+		y_test = np.array(y_test)
+		y_val = np.array(y_val)
 
-    def run(self, df_source_info, df_fluxes):
-        X, y = self._prepare_data(df_source_info, df_fluxes)
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
-
-        scaler = StandardScaler()
-
-        X_train = scaler.fit_transform(X_train)
-        X_test = scaler.transform(X_test)
-        X_val = scaler.transform(X_val)
-
-        X_train = np.expand_dims(X_train, axis=2)
-        X_test = np.expand_dims(X_test, axis=2)
-        X_val = np.expand_dims(X_val, axis=2)
-
-        y_train = np.array(y_train)
-        y_test = np.array(y_test)
-        y_val = np.array(y_val)
-
-        self._fit(X_train, y_train, X_test, y_test, X_val, y_val)
+		self._fit(X_train, y_train, X_test, y_test, X_val, y_val)
 
 def main():
     df_fluxes = pd.read_hdf('data/sdss/preprocessed/balanced_spectral_lines.h5', key='fluxes')
