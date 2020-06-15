4ee375e8c5cb2d3e9ee30d71b1d085186333647a
Andrew <andris788@gmail.com>
2020-06-15 10:41:35 +0200

    (cnn): removed kernel size from hyperparameters

--- spectral_analysis/classifiers/neural_network/cnn.py
+++ spectral_analysis/classifiers/neural_network/cnn.py
@@ -84,7 +84,7 @@ class CNN:
         hyperparameters = {
             'n_conv_layers': hp.Int('n_conv_layers', 3, 6),
             'input_conv_layer_filters': hp.Choice('input_conv_layer_filters', values=[32, 64, 128, 256, 512], default=256),
-            'input_conv_layer_kernel_size': hp.Choice('input_conv_layer_kernel_size', values=[3, 5, 7, 9]),
+            'input_conv_layer_kernel_size': hp.Choice('input_conv_layer_kernel_size', values=[5]),
             'n_dense_layers': hp.Int('n_dense_layers', 2, 6),
             'learning_rate': hp.Choice('learning_rate', values=[1e-3, 1e-4])
         }
@@ -94,7 +94,7 @@ class CNN:
                                                                    values=[64, 128, 256, 512],
                                                                    default=256)
             hyperparameters[f'conv_layer_{i}_kernel_size'] = hp.Choice(f'conv_layer_{i}_kernel_size',
-                                                                       values=[3, 5, 7, 9])
+                                                                       values=[5])
         for i in range(hyperparameters['n_dense_layers']):
             hyperparameters[f'dense_layer_{i}_nodes'] = hp.Choice(f'dense_layer_{i}_nodes',
                                                                    values=[64, 128, 256, 512],
